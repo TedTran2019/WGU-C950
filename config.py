34bafd8309e37ac5c2133d6f39d5a958e3e57e54
Ted Tran <tedtran2019@gmail.com>
2023-03-06 00:04:57 -0800

    Fixed most of time by editing it inside of the config file, still need to change the calculations in Truck

--- config.py
+++ config.py
@@ -1,31 +1,15 @@
-CURRENT_TIME = "08:00"
+from datetime import datetime, time, timedelta
+
+CURRENT_TIME = time(hour=8)
 
 
 def parse_time(time_string):
-    hours, minutes = time_string.split(':')
-    meridiem = minutes[3:]
-    minutes = minutes[:2]
-    if len(hours) == 1:
-        hours = '0' + hours
-    if meridiem == 'PM':
-        hours = int(hours) + 12
-    return str(hours) + ':' + minutes
+    return datetime.strptime(time_string, '%I:%M %p').time()
 
 
-def increment_time(time_string, minutes_added):
-    hours, minutes = time_string.split(':')
-    minutes = int(minutes) + minutes_added
-    hours = int(hours)
-    while minutes >= 60:
-        minutes -= 60
-        hours += 1
-    while hours >= 24:
-        hours -= 24
-    if hours < 10:
-        hours = '0' + str(hours)
-    if minutes < 10:
-        minutes = '0' + str(minutes)
-    return str(hours) + ':' + str(minutes)
+def increment_time(time, minutes_added):
+    delta = timedelta(minutes=minutes_added)
+    return (datetime.combine(datetime.min, time) + delta).time()
 
 
 def increment_global_time(minutes):
